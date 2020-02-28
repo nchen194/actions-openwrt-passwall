1ee29b769f9101b77cef7ba7b3c6cbec77192408
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-02-28 20:37:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,5 +10,7 @@
 sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.$((addr_offset++)).1/10.10.$((addr_offset++)).4/g' package/base-files/files/bin/config_generate
 echo -e "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #cd openwrt && ./scripts/feeds update -a && ./scripts/feeds install -a
+echo -e "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y" >> .config
+echo -e "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y" >> .config
