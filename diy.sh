2ec81126578a0b7048677eac3e1cb20bc007fee3
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-02-28 19:27:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/10.10.$((addr_offset++)).4/g' package/base-files/files/bin/config_generate
+echo -e "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+cd openwrt && ./scripts/feeds update -a && ./scripts/feeds install -a
